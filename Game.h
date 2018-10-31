73154b53fcd9913a781a226925c94e18c8629f1a
Brandon Wolfson <bwolfson97@gmail.com>
2018-10-31 09:38:28 +0000

    Add files via upload

---
+++ Game.h
@@ -0,0 +1,44 @@
+#ifndef GAME_INCLUDED
+#define GAME_INCLUDED
+
+#include "Well.h"
+#include "Piece.h"
+#include "UserInterface.h"
+#include <string>
+
+enum SpecialControls {QUIT, SPACEBAR, DOWN, NORMAL, NOTHING};
+
+class Game
+{
+  public:
+    Game(int width, int height);
+    void play();
+	// Plays a game of Chetyris.
+	
+  private:
+	// Class invariant:
+	//	m_well is the well of width and height passed by the user to the Game constructor
+	//	m_screen is the screen that the user sees
+	//	m_level > 1
+	//	m_score >= 0
+	//	m_rowsLeft is 5 * m_level, at the start of each level. m_rowsLeft >= 0
+
+    Well    m_well;
+    Screen  m_screen;
+    int     m_level;
+	int		m_score;
+	int		m_rowsLeft;
+
+	// Helper functions
+	bool playOneLevel();
+	void displayPrompt(std::string s);
+	void displayStatus(PieceType nextPiece = NONE);
+	void updateStats(int rowsVaporized);
+	void printNextPiece(PieceType nextPiece);
+	void printRightJustified(int stat, int x, int y);
+	bool pieceInPlay(PieceType current);
+	// Returns false if the player quits the game.
+	SpecialControls controls(char move, PieceType current);
+};
+
+#endif // GAME_INCLUDED
---
+++ Piece.h
@@ -0,0 +1,73 @@
+#ifndef PIECE_INCLUDED
+#define PIECE_INCLUDED
+
+#include <string>
+#include <vector>
+using std::vector;
+
+const int BOX_LEN = 4;
+enum PieceType {
+    PIECE_I, PIECE_L, PIECE_J, PIECE_T, PIECE_O, PIECE_S, PIECE_Z,
+    PIECE_VAPOR, PIECE_FOAM, PIECE_CRAZY, NUM_PIECE_TYPES, NONE
+};
+
+class Piece
+{
+public:
+	Piece(PieceType piece);
+	virtual ~Piece() {}
+
+	// Mutators
+	void prevOrientation() { m_orientation--; }
+	void nextOrientation() { m_orientation++; }
+	void moveLeft() { m_x--; }
+	void moveRight() { m_x++; }
+	void moveDown() { m_y++; }
+
+	// Accessors
+	int getOrientation() const { return m_orientation; }
+	int getX() const { return m_x; }
+	int getY() const { return m_y; }
+	char getBox(int row, int col) const { return m_boxOrientations[m_orientation % 4][row][col]; }
+	// Returns the character in a row, column of a piece's bounding box.
+	virtual bool performSpecialAction(vector<vector<char>>& board) const { return false; }
+
+private:
+	// Class invariant:
+	//	m_boxOrientations is an array of the 4 possible orientations of a piece
+	//		in its bounding box
+	//	0 <= m_orientation, m_orientation % 4 yields the current orientation
+	//	m_y, m_x refers to the row and col of the upper left corner of a piece's bounding box,
+	//		on a well.
+	int m_x;
+	int m_y;
+	char m_boxOrientations[4][BOX_LEN][BOX_LEN];
+	int m_orientation;
+
+	// Helpers
+	void createBoxes(const char box0[BOX_LEN][BOX_LEN],const char box1[BOX_LEN][BOX_LEN] = nullptr,
+	const char box2[BOX_LEN][BOX_LEN] = nullptr, const char box3[BOX_LEN][BOX_LEN] = nullptr);
+};
+
+class VaporBomb : public Piece
+{
+public:
+	VaporBomb() : Piece(PIECE_VAPOR) {}
+	virtual bool performSpecialAction(vector<vector<char>>& board) const;
+	// Vaporizes blocks two spaces above and below the vapor bomb.
+};
+
+class FoamBomb : public Piece
+{
+public:
+	FoamBomb() : Piece(PIECE_FOAM) {}
+	virtual bool performSpecialAction(vector<vector<char>>& board) const;
+	// Fills all reachable, open spaces within a 5x5 bounding box 
+	//	surrounding the foam bomb into *'s.
+
+private:
+	void foamBombHelper(vector<vector<char>>& board, int x, int y) const;
+};
+PieceType chooseRandomPieceType();
+
+#endif // PIECE_INCLUDED
